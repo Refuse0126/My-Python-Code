eca4836f3eadafa09feda991240c7aa76eba2663
Refuse0126 <50523719+Refuse0126@users.noreply.github.com>
2019-05-17 19:27:36 +0800

    Update AttrDisplay.py

--- Moudles/Tool/AttrDisplay.py
+++ Moudles/Tool/AttrDisplay.py
@@ -1,13 +1,15 @@
 """
-工具类：重写__str__，以打印类的所有属性，如果以后写的类，需要此功能，直接继承这个工具类即可
+工具类：重载__str__，以打印类的所有属性，如果以后写的类，需要此功能，直接继承这个工具类即可
 """
 class AttrDisplay:
     "a common display tool"
     def gatherAttrs(self):
         attrs=[]
+        #这里只是获得实例对象的属性（在self这个名称空间内通过self.x创建）并不会获得从类那里继承而来的属性，
+        #属性字典__dict__和dir的区别，看自己笔记
         for key in sorted(self.__dict__):
             attrs.append('%s=%s'%(key,getattr(self,key)))
-        return ','.join(attrs)
+        return ','.join(attrs)  #返回一个包含所有属性键值的字符串，并以','隔开
     def __str__(self):
         return '[%s: %s]'%(self.__class__.__name__,self.gatherAttrs())
 
