237cda72d724247bb3b420d81e263913b6bf6f3f
Refuse0126 <50523719+Refuse0126@users.noreply.github.com>
2019-06-09 15:51:34 +0800

    Update key_parameter.py

--- Test/key_parameter.py
+++ Test/key_parameter.py
@@ -12,7 +12,21 @@ def fun():
     import operator
     sort2=sorted(student,key=operator.itemgetter('name'))#相比lambda用法效率更快
     print(student,list(sort1),list(sort2),sep='\n')
+#key参数也同样适用于对类实例针对某个标记进行查询
+class Test():
+    def __init__(self,id):
+        self.id=id
+    def __repr__(self):        #这里注意，__str__只在用户层面才有作用，底层层面都是调用__repr__
+        return '%d'%(self.id)
+
+def fun2():
+    test=[Test(2),Test(1),Test(3)]
+    sorte1=sorted(test,key=lambda k:k.id)            #lambda还是通用
+    import operator
+    sorte2=sorted(test,key=operator.attrgetter('id'))#接口不同，用法类似
+    print(test,list(sorte1),list(sorte2),sep='\n')
 
 if __name__=='__main__':
     fun()
+    fun2()
     
