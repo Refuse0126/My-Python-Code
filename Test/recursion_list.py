df11ec09c07c095f14d92b1631aa64171818507f
Refuse0126 <50523719+Refuse0126@users.noreply.github.com>
2019-06-11 21:11:33 +0800

    Update recursion_list.py

--- Test/recursion_list.py
+++ Test/recursion_list.py
@@ -2,14 +2,15 @@
 用递归实现遍历嵌套列表，还没想到一种能万能遍历方式def recursion(fun,Date)，能够实现遍历，并执行fun函数,
 Date：2019.5.16
 """
-def recursion_list(l):
-    sum=0
+def recursion_list(l):              #1.自定义递归函数一层一层解析
+    result=[]
     for item in l:
         if isinstance(item,list):
-            sum+=recursion_list(item)
+            for i in recursion_list(item):
+                result.append(i)
         else:
-            sum+=item
-    return sum
+            result.append(item)
+    return result
     
 def main():
     a=[1,[2,3],[4,5,6],[7,8,9,10]]
